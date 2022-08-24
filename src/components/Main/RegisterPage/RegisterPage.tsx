70fc13dae95c8f64ba08b35dd086a64a7f97ad07
SeeYou1ater <Sergeyharechkov8@mail.ru>
2022-08-24 10:49:44 +0300

    add controlled inputs in RegisterPage

--- src/components/Main/RegisterPage/RegisterPage.tsx
+++ src/components/Main/RegisterPage/RegisterPage.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { registerUserThunkCreator } from "../../../redux/appReducer"
 import { AppDispatchType } from "../../../redux/redux-store"
 
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const dispatch: AppDispatchType = useDispatch()
 
   const submit = (e: any) => {
@@ -18,8 +22,8 @@ const RegisterPage = () => {
   return (
     <form action="" onSubmit={submit}>
       <h2>Register</h2>
-      <input type="text" name='email' placeholder='email'/>
-      <input type='password' name='password' placeholder="password"/>
+      <input type="text" name='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <input type='password' name='password' placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
       <button>Register</button>
     </form>
   )
