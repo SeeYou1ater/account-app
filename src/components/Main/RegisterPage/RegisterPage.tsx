0c918fd6d7c5bbd9890e7a1ecd0aa36b94232ff7
SeeYou1ater <Sergeyharechkov8@mail.ru>
2022-08-25 12:26:43 +0300

    add registration check auth

--- src/components/Main/RegisterPage/RegisterPage.tsx
+++ src/components/Main/RegisterPage/RegisterPage.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
+import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux"
 import { NavLink } from "react-router-dom"
 import { registerUserThunkCreator } from "../../../redux/appReducer"
-import { AppDispatchType } from "../../../redux/redux-store"
+import { AppDispatchType, RootStateType } from "../../../redux/redux-store"
+import Menu from "../../common/Menu"
 import './RegisterPage.css'
 
 
 const RegisterPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const isAuth = useSelector( (state: RootStateType) => { return state.isAuth} )
 
   const dispatch: AppDispatchType = useDispatch()
 
@@ -21,7 +25,11 @@ const RegisterPage = () => {
     dispatch(registerUserThunkCreator(dataSubmit))
   }
 
-  return (
+  if (isAuth) { return  <div>
+                          <Menu/>
+                          <p className="before-reg-style">Before registration you need to logout!</p>
+                        </div> 
+  } else return (
     <div>
       <p className="menu"><NavLink to="/*">Menu</NavLink></p>
       <form action="" onSubmit={submit} className='register-form form'>
