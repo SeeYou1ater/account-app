8ac5029926bff63ad99b0371209028b52263b613
SeeYou1ater <Sergeyharechkov8@mail.ru>
2022-09-20 18:51:54 +0300

    add test for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,19 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './redux/redux-store';
 
+
+it('App renders', () => {
+  render( <BrowserRouter>
+            <Provider store={store}>
+              <App />
+            </Provider>
+          </BrowserRouter>)
+  
+  expect(screen.getByText('Menu')).toBeInTheDocument()
+  expect(screen.getByText('Contacts')).toBeInTheDocument()
+  expect(screen.getByText('Login')).toBeInTheDocument()
+  expect(screen.getByText('Register')).toBeInTheDocument()
+});
